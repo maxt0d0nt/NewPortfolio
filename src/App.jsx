aa5106990b979c791aa1f12c978f52de22d57f92
maxt0d0nt <maxbox21@hotmail.com>
2024-01-26 10:52:01 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss";
+import Navbar  from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+  <section>Services</section>
+  <serction>Parallax</serction>
+  <section>portfolio1</section>
+  <section>portfolio2</section>
+  <section>portfolio3</section>
+<section>Contact</section>
+  </div>;
 };
 
 export default App;
