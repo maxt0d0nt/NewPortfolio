b1095dce6282d7496dc82aa35333fe93a93fd62c
maxt0d0nt <maxbox21@hotmail.com>
2024-06-25 18:37:08 +0200

    Portfolio done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -37,15 +37,15 @@ const Single = ({item}) => {
 
     const {scrollYProgress} = useScroll({target:ref, })
 
-    //const y= useTransform(scrollYProgress, [0, 1], [-300, 300])
+    const y= useTransform(scrollYProgress, [0, 1], [-200, 200])
     return(
-        <section ref={ref}>
+        <section >
             <div className="container">
                 <div className="wrap">
-                    <div className="imageContainer">
+                    <div className="imageContainer" ref={ref}>
                 <img src={item.img} alt="" />
                 </div>
-                <motion.div className="textContainer" >
+                <motion.div className="textContainer" style={{y}}>
                     <h2>{item.title}</h2>
                     <p>{item.desc}</p>
                     <button>See Demo</button>
